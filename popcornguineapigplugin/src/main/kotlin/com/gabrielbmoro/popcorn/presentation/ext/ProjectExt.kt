cc70a80649b1a39b670af5d233fbec81acbe6c3d
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-09-30 18:19:32 -0300

    Improve logging

--- popcornguineapigplugin/src/main/kotlin/com/gabrielbmoro/popcorn/presentation/ext/ProjectExt.kt
+++ popcornguineapigplugin/src/main/kotlin/com/gabrielbmoro/popcorn/presentation/ext/ProjectExt.kt
@@ -13,15 +13,12 @@ internal fun Project.internalProjectDependencies(
     logger.log(LogLevel.INFO, "PopcornGp: Project group name is $projectGroupName")
 
     project.configurations.onEach { conf ->
-        logger.log(LogLevel.INFO, "PopcornGp: Checking configuration ${conf.name}")
-
         if (conf.name == configurationName) {
             logger.log(LogLevel.INFO, "PopcornGp: Checking configuration [${conf.name}, $configurationName]")
 
             conf.dependencies.map { dep ->
-                logger.log(LogLevel.INFO, "PopcornGp: Checking dependency ${dep.name}")
-
                 dep?.let { safeDep ->
+                    logger.log(LogLevel.INFO, "PopcornGp: ${safeDep.group}:${safeDep.name} <--> $projectGroupName")
                     if (safeDep.group?.contains(projectGroupName) == true) {
 
                         logger.log(LogLevel.INFO, "PopcornGp: Dependency ${dep.name} is internal")
