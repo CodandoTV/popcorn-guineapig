a277661b3420f58e42b1c42db7cf3bed8181de03
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-12-29 21:48:47 -0300

    Add some unit tests + solve calendar extension bug

--- popcornguineapigplugin/src/main/kotlin/com/github/codandotv/popcorn/presentation/ext/CalendarExt.kt
+++ popcornguineapigplugin/src/main/kotlin/com/github/codandotv/popcorn/presentation/ext/CalendarExt.kt
@@ -3,13 +3,12 @@ package com.github.codandotv.popcorn.presentation.ext
 import java.util.Calendar
 
 internal fun Calendar.dateTimestamp(): String {
-    val calendar = Calendar.getInstance()
-    val year = calendar.get(Calendar.YEAR)
-    val month = calendar.get(Calendar.MONTH)
-    val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
-    val hourOfDay = calendar.get(Calendar.HOUR_OF_DAY)
-    val minute = calendar.get(Calendar.MINUTE)
-    val second = calendar.get(Calendar.SECOND)
+    val year = get(Calendar.YEAR)
+    val month = get(Calendar.MONTH)
+    val dayOfMonth = get(Calendar.DAY_OF_MONTH)
+    val hourOfDay = get(Calendar.HOUR_OF_DAY)
+    val minute = get(Calendar.MINUTE)
+    val second = get(Calendar.SECOND)
 
     return "$year-$month-$dayOfMonth" + "_" + "$hourOfDay-$minute-$second"
 }
