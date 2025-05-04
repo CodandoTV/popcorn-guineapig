1592d5da33cd5ed5d2840423d7acc559a2a7b7fb
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-05-04 19:06:26 -0300

    Remove test comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kover) apply false
-// 1. Example apply the parent plugin
-//    id("io.github.codandotv.popcorngp.parent")
 }
 
 buildscript {
@@ -23,23 +21,3 @@ buildscript {
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
 }
-
-
-/*
-// 1. Example apply the parent plugin
-popcornGuineapigParentConfig {
-    type = ProjectType.JAVA
-    groupName = null
-    children = listOf(
-        PopcornChildConfiguration(
-            rules = listOf(
-                NoDependencyRule(),
-                DoNotWithRule(
-                    notWith = listOf("[a-z]+-data")
-                )
-            ),
-            skippedRules = listOf(NoDependencyRule::class),
-            moduleNameRegex = "[a-z]+-presentation"
-        )
-    )
-}*/
