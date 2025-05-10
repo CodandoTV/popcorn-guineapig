b75e7bb1c2e1cbf91ac69aa7888f165649fad558
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-05-10 20:53:15 -0300

    Remove unused parameter from PopcornChildConfiguration

--- popcornguineapigplugin/src/main/kotlin/com/github/codandotv/popcorn/domain/input/PopcornChildConfiguration.kt
+++ popcornguineapigplugin/src/main/kotlin/com/github/codandotv/popcorn/domain/input/PopcornChildConfiguration.kt
@@ -1,10 +1,8 @@
 package com.github.codandotv.popcorn.domain.input
 
 import com.github.codandotv.popcorn.domain.rules.PopcornGuineaPigRule
-import kotlin.reflect.KClass
 
 data class PopcornChildConfiguration(
     val moduleNameRegex: String,
     val rules: List<PopcornGuineaPigRule>,
-    val skippedRules: List<KClass<*>>?,
 )
